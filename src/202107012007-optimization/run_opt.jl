b4220c5adba99ae08f7f50f07386e0c0b13984b0
Jared J. Thomas <jaredthomas68@gmail.com>
2021-10-27 16:38:31 -0600

    updates for testing

--- src/202107012007-optimization/run_opt.jl
+++ src/202107012007-optimization/run_opt.jl
@@ -3,5 +3,5 @@ include("202107012007-optimization.jl")
 run_optimization_series(1, "low-ti", "sowfa-nrel", "./low-ti-aec-no-wec/", 
 "angle-each-circle", wec=true, lspacing=5.0, firstrun=1, verbose=true, plotresults=false)
 
-run_optimization_series(400, "low-ti", "sowfa-nrel", "./low-ti-aec-no-wec/", 
-"angle-each-circle", wec=true, lspacing=5.0, firstrun=1, verbose=true, plotresults=false)
+#run_optimization_series(400, "low-ti", "sowfa-nrel", "./low-ti-aec-no-wec/", 
+#"angle-each-circle", wec=true, lspacing=5.0, firstrun=1, verbose=true, plotresults=false)
