b4a7224520f18ce5e278ae7abfe9bd1f7e1bf7cc
jaredthomas68 <jaredthomas68@gmail.com>
2021-12-13 14:12:18 -0700

    include files mimicking a past error for testing

---
+++ src/202107012007-optimization/run_opt_wrong.jl
@@ -0,0 +1,40 @@
+using ArgParse
+
+include("202107012007-optimization.jl")
+
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table! s begin
+        "--firstrun"
+            help = "which layout to start with"
+            arg_type = Int
+            default = 1
+        "--nruns"
+            help = "how many layouts to run in this set"
+            arg_type = Int
+            default = 1
+        "--case"
+            help = "either high-ti or low-ti"
+            default = "low-ti"
+            arg_type = String
+        "--out-dir"
+            help = "where to put generated files"
+            arg_type = String
+            default = "./"
+    end
+
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_commandline()
+    println("Parsed args:")
+    for (arg,val) in parsed_args
+        println("  $arg  =>  $val")
+    end
+
+    run_optimization_series(parsed_args["nruns"], parsed_args["case"], "sowfa-nrel-wrong-ws", outdir=parsed_args["out-dir"], layoutgen="angle-each-circle", wec=true, lspacing=5.0, firstrun=parsed_args["firstrun"], verbose=true, plotresults=false, savehistory=true)
+
+end
+
+main()
