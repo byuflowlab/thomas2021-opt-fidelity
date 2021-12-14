867e59204569e53908ffb6ebf20c071be497ddd9
jaredthomas68 <jaredthomas68@gmail.com>
2021-12-13 17:11:34 -0700

    missed files from last commit

---
+++ src/202112071550-directional-fidelity/run_opt_high_ti.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=4G   # memory per CPU core
+#SBATCH -J "LES low-ti dir fidelity"   # job name
+#SBATCH --array=0-16     # job array of size 200
+
+echo ${SLURM_ARRAY_TASK_ID}
+
+# load julia module
+module load julia
+
+# set up
+dirbins=(5 10 15 20 30 40 50 70 90 110 140 170 200 240 280 320 360)
+
+# run julia
+julia run_opt.jl --firstrun 1 --nruns 100 --case "high-ti" --out-dir "./high-ti/" --dir-bins ${dirbins[${SLURM_ARRAY_TASK_ID}]}
